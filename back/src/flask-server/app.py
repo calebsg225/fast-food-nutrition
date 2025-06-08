9661bf9f8af27ecf6df538b5da8584bdf6e47b63
kevin <kevin3410@gmail.com>
2025-06-08 01:31:32 -0400

    using post() in flask server

--- back/src/flask-server/app.py
+++ back/src/flask-server/app.py
@@ -1,32 +1,42 @@
-from flask import Flask
+from flask import Flask, request
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/add-restaurant/<name>')
-def add_restaurant(name):
-    # code to add new restaurant to DB
-    return f"Added {escape(name)}, Restaurant ID: x"
+@app.post('/add-restaurant')
+def add_restaurant():
+    data = request.get_json()
+    name = data['name']
+    # add restaurant to DB
+    id = 1414
+    return f"Added {escape(name)}, Restaurant ID: {id}"
 
-@app.route('/delete-restaurant/<name>')
-def delete_restaurant(name):
-    # code to delete restaurant from DB
-    return f"Deleted {escape(name)}, Restaurant ID: x"
+@app.route('/delete-restaurant/<int:id>')
+def delete_restaurant(id):
+    # get restaurant name from DB
+    name = "pizza-hut"
+    # delete restaurant from DB
 
-@app.route('/add-item/<restaurant_id>/<name>')
-def add_menu_item(restaurant_id, name):
-    # code to add new item to DB
-    return f"Added {escape(name)} to {escape(restaurant_id)}, Menu Item ID: x"
+    return f"Deleted {escape(name)}, Restaurant ID: {escape(id)}"
 
-@app.route('/set-calories/<menu_item_id>/<calories>')
-def set_menu_item_calories(menu_item_id, calories):
-    # code to set calories for menu item
-    return f"Set calories for {escape(menu_item_id)} to {escape(calories)}"
+@app.post('/add-menu-item')
+def add_menu_item():
+    data = request.get_json()
+    restaurant_id = data['restaurant_id']
+    menu_item = data['item']
+    # add new item to DB
+    menu_item_id = 1414
+    return f"Added {escape(menu_item['name'])} to {escape(restaurant_id)}, Menu Item ID: {menu_item_id}"
 
-@app.route('/get-nearest-restaurants/<latitude>/<longitude>')
-def get_nearest_restaurants(latitude, longitude):
-    # code to get nearest restaurants
-    return f"Nearest Restaurants from {escape(latitude)}, {escape(longitude)}: x"
+@app.post('/get-nearest-restaurants')
+def get_nearest_restaurants():
+    lat = request.args.get('lat', 181)
+    long = request.args.get('long', 181)
+    if lat < -90 or lat > 90 or long < -180 or long > 180:
+        return f"Coordinates ({lat}, {long}) are invalid."
+    # get nearest restaurants
+    restaurants = []
+    return restaurants
 
 if __name__ == '__main__':
     app.run()
